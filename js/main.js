6b9f403bec62c0b18375bdfca6f2516891635b47
satanaroom <oficialbalunov@gmail.com>
2020-08-13 15:39:11 +0800

    fixed: footer

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ $(document).ready(function () {
   menuButton.on("click", function () {
     $(".navbar-bottom").toggleClass("navbar-bottom--visible");
   });
+
+  var menuButton = document.querySelector(".header__image");
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".header-menu")
+      .classList.toggle("header-menu--visible");
+  });
+
+  var menuButton = document.querySelector(".header-menu__button");
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".header-menu")
+      .classList.toggle("header-menu--visible");
+  });
 });
