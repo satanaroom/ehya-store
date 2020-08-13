b5238cf921263c23c0c4adb41f9e16d24b93e8ed
satanaroom <oficialbalunov@gmail.com>
2020-08-14 03:39:01 +0800

    create: 50% trending section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-  var menuButton = $(".menu-button");
-  menuButton.on("click", function () {
-    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
-  });
-
+  // Мобильное меню
   var menuButton = document.querySelector(".header__image");
   menuButton.addEventListener("click", function () {
     document
@@ -16,5 +12,31 @@ $(document).ready(function () {
     document
       .querySelector(".header-menu")
       .classList.toggle("header-menu--visible");
+  });
+
+  // Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      errorElement: "em",
+      messages: {
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+      },
+    });
+  });
+
+  // Переключение табов
+  var tabsItem = $(".trending__item");
+  var contentItem = $(".trending__cards");
+
+  tabsItem.on("click", function (event) {
+    var activeContent = $(this).attr("data-target");
+    tabsItem.removeClass("trending__item--active");
+    contentItem.removeClass("trending__cards--active");
+    $(activeContent).addClass("trending__cards--active");
+    $(this).addClass("trending__item--active");
   });
 });
